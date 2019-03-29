ab0141eed63135593e7a99be54e92de695d00822
Luisa Arevalo <00038617@uca.edu.sv>
2019-03-29 15:32:39 -0600

    Adelanto

--- app/src/main/java/com/example/evaluacion1_pdm_00038617/second.java
+++ app/src/main/java/com/example/evaluacion1_pdm_00038617/second.java
@@ -1,13 +1,23 @@
 package com.example.evaluacion1_pdm_00038617;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class second extends AppCompatActivity {
+
+    private TextView prod_1, prod_2, prod_3,prod_4,prod_5,prod_6,prod_7,prod_8,prod_9, email, username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent intento = this.getIntent();
+
+        if (intento!=null) {
+
+        }
     }
+
 }
